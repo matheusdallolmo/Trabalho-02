9dbb92c8a88bd9d7476acd1a5ee78b4033fa1b0a
Lorenzo Benetti <102761285+Benetti03@users.noreply.github.com>
2023-11-16 16:32:11 -0300

    Add files via upload

--- Campeonato.java
+++ Campeonato.java
@@ -8,19 +8,19 @@ import java.io.File;
 
 public class Campeonato {
     private Scanner teclado = new Scanner(System.in);
-    private File arquivo = new File("JogoGeneral.dat");
+    private File arquivo = new File("JogoDeApostas.dat");
     private Jogador[] jogadores = new Jogador[10];
     private int quantJog = 0, i;
 
     //Funcao para incluir jogador
     public void incluirJogador(){ 
-        // Validacao do limite de jogadores
+        // Caso o limite de jogadores seja atingido, imprime uma mensagem de limite de jogadores e roda o menu novamente
         if(quantJog != 10){ 
             String nome;
             char tipoJogador;
             
             // Solicitar nome do jogador
-            System.out.println("-> Informe o nome do novo Jogador: ");
+            System.out.println("->Informe o nome do novo Jogador: ");
             nome = teclado.nextLine();
             
             // Solicitar tipo do jogador
@@ -30,58 +30,72 @@ public class Campeonato {
                 teclado.nextLine();
                 if(tipoJogador != 'h' && tipoJogador != 'H' && tipoJogador != 'm' && tipoJogador != 'M')
                     System.out.println("Tipo de jogador incorreto, digite a opcao novamente (H,M) ou (h,m)\n");
+
             }while(tipoJogador != 'h' && tipoJogador != 'H' && tipoJogador != 'm' && tipoJogador != 'M'); //Do while para que a pessoa selecione apenas opcoes corretas H ou M (tanto maiusculo quanto minusculo)
 
-            // Iniciar o jogador de acordo com seu tipo
-            if(tipoJogador != 'h' || tipoJogador != 'H')
-                jogadores[quantJog] = new Humano(nome);
+            if(tipoJogador == 'h' || tipoJogador == 'H'){
+                System.out.println("Informe o CPF do jogador: ");
+                String cpf = teclado.nextLine();
+                System.out.println("Informe a agencia do jogador: ");
+                String agencia = teclado.nextLine();
+                System.out.println("Informe a conta do jogador: ");
+                String conta = teclado.nextLine();
+                System.out.println("Informe o numero do banco do jogador: ");
+                int numeroBanco = teclado.nextInt();
+                teclado.nextLine();
+
+                jogadores[quantJog] = new Humano(nome, cpf, agencia, conta, numeroBanco);
+            }
+                
             else
                 jogadores[quantJog] = new Maquina(nome);
 
-            // Salvar a quantidade de jogadores ja adicionados
             quantJog++;
             System.out.println("Jogador "+nome+" adicionado com sucesso!\n");
             
+            
         }else
             System.out.println("Limite de jogadores alcancado! Inicie o campeonato.\n");
-    }       
+    }
 
     //Funcao para remover jogador
-    public void removerJogador(){ 
-        String remover;
-        int opcao = -1;
+    public void removerJogador(){
+        // Caso nao haja jogadores, imprime uma mensagem de erro e roda o menu novamente
+        if(quantJog == 0)
+            System.out.println("Nao ha jogadores para remover!\n");
+        else{
+            String nome;
+            int indice = -1;
 
-        // Mostrar a lista de jogadores
-        System.out.println("-> Lista de Jogadores: ");
-        for(i = 1; i <= quantJog; i++){ 
-            System.out.println(jogadores[i - 1].getNome());
+            System.out.println("-> Lista de Jogadores: ");
+            for(i = 1; i <= quantJog; i++){ 
+                System.out.println(i + "-> " + jogadores[i - 1].getNome());
+            }
+
+            // Solicitar nome do jogador
+            do{
+                System.out.println("Informe o nome do jogador que deseja remover: ");
+                nome = teclado.nextLine();
+
+                // Procurar o jogador no vetor de jogadores
+                for(i=0; i<quantJog; i++){
+                    if(jogadores[i].getNome().equals(nome)){
+                        indice = i;
+                        break;
+                    }
+                }
+                // Caso o jogador nao seja encontrado, imprime uma mensagem de erro e roda o menu novamente
+                if(indice == -1)
+                    System.out.println("Jogador nao encontrado!\n");
+
+            }while(indice == -1);
+
+                for(i=indice; i<quantJog-1; i++)
+                    jogadores[i] = jogadores[i+1];
+                
+                quantJog--;
+                System.out.println("Jogador "+nome+" removido com sucesso!\n");
         }
-
-        // Solicitar qual jogador deve ser removido
-        do{
-            System.out.print("Indique o nome do jogador a ser removido: ");
-            remover = teclado.nextLine();
-            for(i = 0; i < quantJog; i++) 
-                if(remover == jogadores[i].getNome());
-                    opcao = i;
-            if(opcao == -1)
-                System.out.println("Jogador não encontrado!");
-        }while(opcao == -1);
-        
-        // Remover o jogador
-        System.out.println("O jogador "+remover+" foi removido com sucesso!\n");
-        for(i = opcao; i < quantJog; i++){ 
-            if(i == 9)
-                jogadores[i] = null;
-            else
-                jogadores[i] = jogadores[i + 1];
-        }
-
-        // Ajuste do tamanho do vetor de jogadores
-        quantJog--; 
-
-        // Limpando buffer para que apos a remocao seja possivel a implementacao de outro jogador sem erros
-        teclado.nextLine(); 
     }
 
     // Funcao para executar rodada(s) de aposta(s)
@@ -106,29 +120,31 @@ public class Campeonato {
 
     //Funcao para gravar em arquivo
     public void gravarEmArquivo(){ 
-        try{
+        try {
             FileOutputStream fout = new FileOutputStream(arquivo);
             ObjectOutputStream oos = new ObjectOutputStream(fout);
-
-            // Gravando o vetor de pessoas no arquivo
+            // gravando o vetor de pessoas no arquivo
             oos.writeObject(jogadores);
             oos.flush();
             oos.close();
             fout.close();
 
             System.out.println("Arquivo gravado com sucesso!");
-        }catch(Exception ex){
-            System.err.println("erro: "+ex.toString());
+        } catch (Exception ex) {
+            System.err.println("erro: " + ex.toString());
         }
     }
 
     //Funcao para ler do arquivo
     public void lerDoArquivo(){ 
-        try{
+        try {
             FileInputStream fin = new FileInputStream(arquivo);
             ObjectInputStream oin = new ObjectInputStream(fin);
-          
-            // Ler os objetos do arquivo e fazer a coercao de tipos
+            /*
+            * Lendo os objetos de um arquivo e fazendo a
+            * coercao de tipos
+            */
+
             Jogador[] jogadoresArq = (Jogador[]) oin.readObject();
             oin.close();
             fin.close();
@@ -145,8 +161,9 @@ public class Campeonato {
             // Informar que o arquivo foi lido com sucesso
             System.out.println("Arquivo lido com sucesso!\n");
 
-        }catch (Exception ex){
-            System.err.println("erro: " + ex.toString());
-        }
+            } catch (Exception ex) {
+                System.err.println("erro: " + ex.toString());
+            }
     }
+
 }
--- Humano.java
+++ Humano.java
@@ -1,12 +1,41 @@
 public class Humano extends Jogador implements JogarComoHumano{
+    private String cpf;
+    private String agencia;
+    private String conta;
+    private int numeroBanco;
 
     public Humano(){
-        super(" ");
+        super();
+        cpf = " ";
+        agencia = " ";
+        conta = " ";
+        numeroBanco = 0;
     }
 
-    public Humano(String nome){
+    public Humano(String nome, String cpf, String agencia, String conta, int numeroBanco){
         super(nome);
+        this.cpf = cpf;
+        this.agencia = agencia;
+        this.conta = conta;
+        this.numeroBanco = numeroBanco;
     }
 
-    
+    public int escolherJogo(){ //tem que achar o lugar certo ainda
+        int opcao = 0;
+
+        System.out.println("Escolha o jogo: ");
+        System.out.println("1 - Jogo de Azar");
+        System.out.println("2 - Jogo de General");
+
+        do{
+            if(opcao != 1 && opcao != 2)
+                System.out.println("Opcao invalida, digite novamente (1 ou 2)\n");
+        }while(opcao != 1 && opcao != 2);
+
+        return opcao;
+    }
+
+    public void escolherJogada(){
+
+    }
 }
